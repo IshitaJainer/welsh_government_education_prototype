6df8ac5201101a01b7b832e25d67a26b312ac4e6
IshitaJainer <ishita.jainer@thepsc.co.uk>
2026-06-01 23:58:04 +0100

    upload of initial files

---
+++ app/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  serviceName: 'My Local School · Fy Ysgol Leol',
+  port: '3000',
+  useAuth: 'false',
+  useAutoStoreData: 'true',
+  useCookieSessionStore: 'false',
+  useDocumentation: 'true',
+  useHttps: 'true',
+  useBrowserSync: 'true'
+}
