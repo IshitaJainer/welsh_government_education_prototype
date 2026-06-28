4f211ee5bfc1fa67848fcc567e355ce6e9e89565
Joshua-AnsahPSC <Joshua.Ansah@thepsc.co.uk>
2026-06-28 22:55:03 +0000

    WG Round 2 Comments implemented also introduced dummy data asset and made some pages dynamicaally pull that data

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+// Read query parameters on GET requests and store in session
+// This replicates the standard Prototype Kit auto-store-data behaviour for GET requests
+router.get('*', function (req, res, next) {
+  var query = req.query
+  if (query && Object.keys(query).length > 0) {
+    req.session.data = req.session.data || {}
+    Object.assign(req.session.data, query)
+  }
+  next()
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
